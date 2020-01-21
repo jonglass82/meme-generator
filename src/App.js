fb03f616b6048691a66553854da4920e28b51714
Tiffany Tychewicz <tychewicz.design.studio@gmail.com>
2020-01-21 14:15:21 -0600

    Got a basic router set up using React Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import MemeGenerator from './components/MemeGenerator'
+import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+render((
+    <Router>
+        <App/>
+    </Router>
+), document.getElementById('root'));
 
 function App() {
   return (
     <div className="App">
-
+ <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="www.google.com">About</Link>
+            </li>
+            <li>
+              <Link to="www.google.com">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
       <Header />
 
       <MemeGenerator/>
