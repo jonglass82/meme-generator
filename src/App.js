6117ad2d36d71b7dcdc943c95200d4e15caba489
Tiffany Tychewicz <tychewicz.design.studio@gmail.com>
2020-01-26 17:24:12 -0600

    built test tabs for navbar. going to start linking them next.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import MemeGenerator from './components/MemeGenerator'
+import NavTab from './components/NavTabs'
+
 
 
 function App() {
   return (
     <div className="App">
+
+      <NavTab />
 
       <Header />
 
